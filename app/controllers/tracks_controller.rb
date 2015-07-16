465484dc8c2c3924d70bf2b60b381debba6129b6
Erez Bosch <erezaka@gmail.com>
2015-07-16 13:57:14 -0700

    finish adding albums

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,60 @@
+class TracksController < ApplicationController
+  before_action :redirect_unless_logged_in
+
+  def new
+    @track = Track.new
+    @track.album_id = params[:album_id]
+    render :new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      redirect_to track_url(@track)
+    else
+      flash[:errors] = @track.errors.full_messages
+      redirect_to new_track_url
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+    render :show
+  end
+
+  def index
+    @tracks = Track.all
+    render :index
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @track = Track.find(params[:id])
+    if @track.update(Track_params)
+      redirect_to track_url(@track)
+    else
+      flash[:errors] = @track.errors.full_messages
+      redirect_to edit_track_url
+    end
+  end
+
+  def destroy
+    @track = Track.find(params[:id])
+    if @track.destroy
+      redirect_to tracks_url
+    else
+      flash[:errors] = ["You can't destroy that!"]
+      redirect_to track_url(@track)
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:name, :album_id, :is_bonus, :lyrics)
+  end
+end
--- app/helpers/bands_helper.rb
+++ app/helpers/bands_helper.rb
@@ -1,2 +1,5 @@
 module BandsHelper
+  def summarize(album)
+    "#{album.name} " + (album.is_live ? "(live)" : "(studio)")
+  end
 end
