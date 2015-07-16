3ccfaa0e8979055cce93457511d9b939b7d053a1
Erez Bosch <erezaka@gmail.com>
2015-07-16 15:37:15 -0700

    authenticate note deletion

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,14 @@
 class NotesController < ApplicationController
+  before_action :ensure_destroy_targets_own_note, only: :destroy
   def destroy
     @note = Note.find(params[:id])
     @track = @note.track
     @note.destroy!
     redirect_to track_url(@track)
+  end
+
+  def ensure_destroy_targets_own_note
+    @note = Note.find(params[:id])
+    render text: "403 FORBIDDEN" unless @note.user == current_user
   end
 end
